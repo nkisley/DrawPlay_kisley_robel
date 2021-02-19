bf070ce7ffb42a942913a2446202f96ec609a7b9
Noah Kisley <nkisley@mymail.mines.edu>
2021-02-18 17:59:35 -0700

    Changed mouth color

--- src/Cat.java
+++ src/Cat.java
@@ -46,7 +46,7 @@ public class Cat {
 		y -= 15;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
+		g2.setColor(Color.cyan);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
